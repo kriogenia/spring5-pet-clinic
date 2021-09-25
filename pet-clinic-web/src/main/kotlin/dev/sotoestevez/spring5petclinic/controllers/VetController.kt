daf406cf26ddb960b4cdce7b32f7770bb3008711
Ricardo Soto Estévez <uo265710@uniovi.es>
2021-09-25 13:34:10 +0200

    List vets. Closes #15

--- pet-clinic-web/src/main/kotlin/dev/sotoestevez/spring5petclinic/controllers/VetController.kt
+++ pet-clinic-web/src/main/kotlin/dev/sotoestevez/spring5petclinic/controllers/VetController.kt
@@ -1,18 +1,26 @@
 package dev.sotoestevez.spring5petclinic.controllers
 
+import dev.sotoestevez.spring5petclinic.services.VetService
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 @RequestMapping("/vet")
-class VetController {
+class VetController(
+	private val service: VetService
+) {
 
 	companion object {
 		private const val ROOT = "vet/"
 		private const val INDEX = "index"
+		private const val VETS = "vets"
 	}
 
 	@RequestMapping("", "/", "/${INDEX}", "/${INDEX}.html")
-	fun list(): String = ROOT + INDEX
+	fun list(model: Model): String {
+		model.addAttribute(VETS, service.findAll())
+		return ROOT + INDEX
+	}
 
 }
