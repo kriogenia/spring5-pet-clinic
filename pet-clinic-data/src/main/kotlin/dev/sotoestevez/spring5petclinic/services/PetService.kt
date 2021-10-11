cb7cf91273ad52ea8dd1f87d2f8183a1d6e5bc2d
Ricardo Soto Estévez <47500377+kriogenia@users.noreply.github.com>
2021-10-11 13:20:22 +0200

    Add PetType to Service. Closes #28

--- pet-clinic-data/src/main/kotlin/dev/sotoestevez/spring5petclinic/services/PetService.kt
+++ pet-clinic-data/src/main/kotlin/dev/sotoestevez/spring5petclinic/services/PetService.kt
@@ -1,7 +1,10 @@
 package dev.sotoestevez.spring5petclinic.services
 
 import dev.sotoestevez.spring5petclinic.model.Pet
+import dev.sotoestevez.spring5petclinic.model.PetType
 
 interface PetService: CrudService<Pet, Long> {
+
+	fun findPetTypes(): List<PetType>
 
 }
--- pet-clinic-data/src/main/kotlin/dev/sotoestevez/spring5petclinic/services/map/PetMapService.kt
+++ pet-clinic-data/src/main/kotlin/dev/sotoestevez/spring5petclinic/services/map/PetMapService.kt
@@ -1,8 +1,13 @@
 package dev.sotoestevez.spring5petclinic.services.map
 
 import dev.sotoestevez.spring5petclinic.model.Pet
+import dev.sotoestevez.spring5petclinic.model.PetType
 import dev.sotoestevez.spring5petclinic.services.PetService
 import org.springframework.stereotype.Service
 
 @Service
-class PetMapService: AbstractMapService<Pet>(), PetService {}
+class PetMapService: AbstractMapService<Pet>(), PetService {
+
+	override fun findPetTypes(): List<PetType> = map.values.map { it.petType }.toList()
+
+}
